c8dcdcc3d47dc8e7abb17598190262211c864781
k karthik <165780918+karthik07605@users.noreply.github.com>
2025-09-06 14:38:49 +0530

    Update settings.py

--- papers/settings.py
+++ papers/settings.py
@@ -130,3 +130,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
