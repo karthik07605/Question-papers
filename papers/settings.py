7c4998dd0359a0041dca6d35e86ee6a99dbc2503
k karthik <165780918+karthik07605@users.noreply.github.com>
2025-09-06 20:52:35 +0530

    Update settings.py

--- papers/settings.py
+++ papers/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sitemaps',
     'django.contrib.staticfiles',
     'data'
 ]
@@ -149,6 +150,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 
 
 
